a3799e42f449df476b940c28aefcaaf95b5e2a6f
Paulo Junior <paulo.junior@jussi.local>
2023-10-21 06:58:47 -0300

    primerio cenario de breadcrumb

--- cypress/e2e/navigation/breadcrumb.cy.js
+++ cypress/e2e/navigation/breadcrumb.cy.js
@@ -1,1 +1,16 @@
-/// testando a branch teste-qa
+///<reference types= "cypress"/> 
+
+describe('orçamento', () => {
+
+    beforeEach('acesso a página', () => {
+      cy.visit('https://project-passaport-qa.vercel.app/prosHub/breadtest2/breadcrumb-test')
+    })
+
+    const iconeHome = "#__next > div > nav > ol > li:nth-child(1) > a > img"
+  
+    it('validar direcionamento ao clicar no ícone de home', () => {
+      
+        cy.get(iconeHome).click();
+        cy.url().should('eq', 'https://project-passaport-qa.vercel.app/');
+    })
+  })
